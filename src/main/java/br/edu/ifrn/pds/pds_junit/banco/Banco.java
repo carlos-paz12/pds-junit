be3294d7361fb31b5569e9226b97ee47b75fefdc
Carlos Paz <147278027+carlos-paz12@users.noreply.github.com>
2023-11-06 12:01:22 -0300

    feat: Cria classe Banco

---
+++ src/main/java/br/edu/ifrn/pds/pds_junit/banco/Banco.java
@@ -0,0 +1,38 @@
+package br.edu.ifrn.pds.pds_junit.banco;
+
+public class Banco {
+
+    public void solicitarEmprestimo(float valorEmprestimo, float taxaJuros) {
+        if (valorEmprestimo >= 3000 && valorEmprestimo <= 8000) {
+
+            // TODO
+
+        }
+        else if (valorEmprestimo > 8000 && valorEmprestimo <= 25000) {
+
+            // TODO
+
+        }
+        else if (valorEmprestimo > 25000 && valorEmprestimo <= 50000) {
+
+            // TODO
+
+        }
+        else if (valorEmprestimo > 50000 && valorEmprestimo <= 150000) {
+
+            // TODO
+
+        }
+    }
+
+    public int mesesParcelados() {
+        // TODO
+        return 0;
+    }
+
+    public float valorParcela() {
+        // TODO
+        return 0;
+    }
+
+}
